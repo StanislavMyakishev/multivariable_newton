8e27fdd811861b43a3c8e90f1be43df39dfa9ddb
StanislavMyakishev <masiff-s@live.ru>
2019-11-29 22:58:01 +0300

    tupovo govna

--- newton-raphson.py
+++ newton-raphson.py
@@ -49,15 +49,12 @@ def newtonRaphson(f, x, gradient, hesseMatrix, eps):
     :return:
     """
     grad = gradient(x[0], x[1])
-    grad = grad / norm(grad)
     curVals = np.array([x[0], x[1]])
     hesse = hesseMatrix(x[0], x[1])
     counter = 0
-    while norm(gradient(curVals[0], curVals[1])) > eps:
+    while norm(grad) > eps:
         counter += 1
-        curVals = np.add(curVals, np.multiply(np.dot(grad, hesse), -1))
-        print(curVals)
-        print(f(curVals))
+        curVals = np.subtract(curVals, np.dot(hesse, grad))
         grad = gradient(curVals[0], curVals[1])
         hesse = hesseMatrix(curVals[0], curVals[1])
     print(counter)
@@ -69,8 +66,8 @@ def reverseHesseMatrix(x1, y1):
     x, y = symbols('x y')
     F = Pow(Pow(x, 2) + y - 11, 2) + Pow(x + Pow(y, 2) - 7, 2)
     return np.power(np.array([[diff(F, x, x).subs([(x, x1), (y, y1)]), diff(F, x, y).subs([(x, x1), (y, y1)])],
-                        [diff(F, x, y).subs([(x, x1), (y, y1)]), diff(F, y, y).subs([(x, x1), (y, y1)])]]), -1)
+                              [diff(F, x, y).subs([(x, x1), (y, y1)]), diff(F, y, y).subs([(x, x1), (y, y1)])]]), -1)
 
 
 if __name__ == '__main__':
-    print(newtonRaphson(f, [4,3], numerical_gradient, reverseHesseMatrix, 0.01))
+    print(newtonRaphson(f, [4, 3], numerical_gradient, reverseHesseMatrix, 0.01))
