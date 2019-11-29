2a4df0845c05ad29ff1461feb5be6620691ea22f
StanislavMyakishev <masiff-s@live.ru>
2019-11-29 20:40:59 +0300

    function 1) added with grad

--- newton-raphson.py
+++ newton-raphson.py
@@ -8,7 +8,7 @@ def f(x):
     :param x: x parameter
     :return: returns the function value
     """
-    return (1.5 - x[0] * (1 - x[1])) ** 2 + (2.25 - x[0] * (1 - x[1] ** 2)) ** 2 + (2.625 - x[0] * (1 - x[1] ** 3)) ** 2
+    return (x[0] ** 2 + x[1] - 11) ** 2 + (x[0] + x[1] ** 2 - 7) ** 2
 
 
 def numerical_gradient(x1, x2, dx=1e-6):
@@ -32,37 +32,17 @@ def analytical_gradient(x1, x2):
     :param x2: second parameter
     :return: analytical form of the gradient for given function
     """
-    derivative_x1 = 2 * (1.5 - x1 * (1 - x2)) * (x2 - 1) + 2 * (2.25 - x1 * (1 - x2 ** 2)) * (x2 ** 2 - 1) \
-                    + 2 * (2.625 - x1 * (1 - x2 ** 3)) * (x2 ** 3 - 1)
-    derivative_x2 = 2 * (1.5 - x1 * (1 - x2)) * x1 + 2 * (2.25 - x1 * (1 - x2 ** 2)) * 2 * x1 * x2 \
-                    + 2 * (2.625 - x1 * (1 - x2 ** 3)) * 3 * (x2 ** 2) * x1
+    derivative_x1 = 2 * (2 * x1 * (x1 ** 2 + x2 - 11) + x1 + x2 ** 2 - 7)
+    derivative_x2 = 2 * (x1 ** 2 + 2 * x2 * (x1 + x2 ** 2 - 7) + x2 - 11)
 
     return np.array([derivative_x1, derivative_x2])
 
 
-def HesseMatrix(x,y):
-    """
-        x1, y1 = symbols('x1 y1')
-        F = (x1**2 + y1 - 11)**2 + (x1 + y1**2 - 7)**2
-        dfdx = diff(lambda x: f(x), x[0])
-        dfdy = diff(F, y1)
-        dfdxdx = diff(dfdx, x1)
-        dfdydy = diff(dfdy, y1)
-        dfdxdy = diff(dfdx, y1)
-        dfdydx = diff(dfdy, x1)
-        a = str(dfdx)
-        return np.array([[dfdxdx, dfdxdy], [dfdydx, dfdydy]])
-    :param x:
-    :param y:
-    :return:
-    """
-    x1, y1 = symbols('x1 y1')
-    F = (x1 ** 2 + y1 - 11) ** 2 + (x1 + y1 ** 2 - 7) ** 2
-    dfdx = diff(F, x1)
-    print(str(dfdx))
-    test = "def fq(x, y):\treturn 4*x1*(x1**2 + y1 - 11) + 2*x1 + 2*y1**2 - 14"
-    fu = eval(test)
-    print(fu(1, 2))
+def hesseMatrix(x,y):
+    x, y = symbols('x y')
+    diff(x)
+    F = (x ** 2 + y- 11) ** 2 + (x + y ** 2 - 7) ** 2
+
 
 if __name__ == '__main__':
-    print(HesseMatrix([1, 2],1))
+    print(hesseMatrix())
