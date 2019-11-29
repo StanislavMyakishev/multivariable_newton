68245d5dd8d4aad4e2259e95e1d225006e2a4cba
StanislavMyakishev <masiff-s@live.ru>
2019-11-29 22:37:31 +0300

    tupoe gavno

--- newton-raphson.py
+++ newton-raphson.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import norm
 from sympy import *
 
 
@@ -22,7 +23,7 @@ def numerical_gradient(x1, x2, dx=1e-6):
     derivative_x1 = (f([x1 + dx, x2]) - f([x1 - dx, x2])) / (2 * dx)
     derivative_x2 = (f([x1, x2 + dx]) - f([x1, x2 - dx])) / (2 * dx)
 
-    return np.array([derivative_x1, derivative_x2])
+    return np.array([round(derivative_x1, 10), round(derivative_x2, 10)])
 
 
 def analytical_gradient(x1, x2):
@@ -32,17 +33,44 @@ def analytical_gradient(x1, x2):
     :param x2: second parameter
     :return: analytical form of the gradient for given function
     """
-    derivative_x1 = 2 * (2 * x1 * (x1 ** 2 + x2 - 11) + x1 + x2 ** 2 - 7)
-    derivative_x2 = 2 * (x1 ** 2 + 2 * x2 * (x1 + x2 ** 2 - 7) + x2 - 11)
+    derivative_x1 = 4 * x1 * (x1 ** 2 + x2 - 11) + 2 * x1 + 2 * x2 ** 2 - 14
+    derivative_x2 = 2 * x1 ** 2 + 4 * x2 * (x1 + x2 ** 2 - 7) + 2 * x2 - 22
 
     return np.array([derivative_x1, derivative_x2])
 
 
-def hesseMatrix(x,y):
+def newtonRaphson(f, x, gradient, hesseMatrix, eps):
+    """
+    :param f:
+    :param x:
+    :param grad:
+    :param hesse:
+    :param eps:
+    :return:
+    """
+    grad = gradient(x[0], x[1])
+    grad = grad / norm(grad)
+    curVals = np.array([x[0], x[1]])
+    hesse = hesseMatrix(x[0], x[1])
+    counter = 0
+    while norm(gradient(curVals[0], curVals[1])) > eps:
+        counter += 1
+        curVals = np.add(curVals, np.multiply(np.dot(grad, hesse), -1))
+        print(curVals)
+        print(f(curVals))
+        grad = gradient(curVals[0], curVals[1])
+        hesse = hesseMatrix(curVals[0], curVals[1])
+    print(counter)
+    print(curVals)
+    return f(curVals)
+
+
+def reverseHesseMatrix(x1, y1):
     x, y = symbols('x y')
-    diff(x)
-    F = (x ** 2 + y- 11) ** 2 + (x + y ** 2 - 7) ** 2
+    F = Pow(Pow(x, 2) + y - 11, 2) + Pow(x + Pow(y, 2) - 7, 2)
+    return np.power(np.array([[diff(F, x, x).subs([(x, x1), (y, y1)]), diff(F, x, y).subs([(x, x1), (y, y1)])],
+                        [diff(F, x, y).subs([(x, x1), (y, y1)]), diff(F, y, y).subs([(x, x1), (y, y1)])]]), -1)
 
 
 if __name__ == '__main__':
-    print(hesseMatrix())
+    print(newtonRaphson(f, [4,3], numerical_gradient, reverseHesseMatrix, 0.01))
